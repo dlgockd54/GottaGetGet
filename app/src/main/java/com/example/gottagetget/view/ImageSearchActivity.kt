7be3b25ba94a9116eb76a3142383ae82ef77c279
hclee <dlgockd45@gmail.com>
2019-08-12 14:40:57 +0900

    Remove SearchView included toolbar

--- app/src/main/java/com/example/gottagetget/view/ImageSearchActivity.kt
+++ app/src/main/java/com/example/gottagetget/view/ImageSearchActivity.kt
@@ -2,23 +2,12 @@ package com.example.gottagetget.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import androidx.appcompat.widget.Toolbar
 import com.example.gottagetget.R
-import kotlinx.android.synthetic.main.activity_image_search.*
 
 class ImageSearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_search)
-        setSupportActionBar(toolbar_search_image as Toolbar)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_search, menu)
-        search_view_image_search.setMenuItem(menu?.findItem(R.id.action_search))
-
-        return true
     }
 }
